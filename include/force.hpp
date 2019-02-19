500e14cce540ad96bdd3e5a7b36fd4224bb9d17e
Minjae Isaac Kwon <minjae.isaac.kwon@gmail.com>
2019-02-20 00:18:18 +0900

    Update force.hpp
    Make function readable

--- include/force.hpp
+++ include/force.hpp
@@ -1,13 +1,17 @@
-
 #ifndef H_FORCE
 #define H_FORCE 1;
 
 #include "TVectorT.h"
+#include "TVectorD.h"
 #include "particle.hpp"
+
+using namespace std;
 
 class force{
 public:
-  virtual TVectorD Evaluate(particle* p1, particle* p2){TVectorD a_ = TVectorD(3); return a_;}
+  virtual TVectorD Evaluate(particle* p1_, particle* p2_){
+    TVectorD a_ = TVectorD(3); cout<<"NULL FORCE"<<endl; return a_;
+  }
 protected:
   force(){}
 private:
