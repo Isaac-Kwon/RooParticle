0fb520ebf71f12fedc0bcdc4f83375d9cb2ce9b8
Minjae Isaac Kwon <minjae.isaac.kwon@gmail.com>
2019-03-02 20:16:12 +0900

    Add file main_3body.cpp

---
+++ test/main_3body.cpp
@@ -0,0 +1,13 @@
+#include "TROOT.h"
+#include "RPConfig.hpp"
+
+#include "event.hpp"
+#include "particle.hpp"
+#include "force.hpp"
+
+#include "iostream"
+
+#include "TROOT.h"
+#include "TTree.h"
+#include "TFile.h"
+#include "TMath.h"
