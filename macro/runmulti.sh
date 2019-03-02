f57e57257ae24b5f0a8aa7945f7890e7b4da7c59
Minjae Isaac Kwon <minjae.isaac.kwon@gmail.com>
2019-03-02 22:24:18 +0900

    multijob ncore optimization

--- macro/runmulti.sh
+++ macro/runmulti.sh
@@ -1,8 +1,17 @@
 
 echo $1 $2 $3
+echo RUN $1 $2 : $3 times
 
-echo RUN $1 $2 times
+njob=10
 
-for (( i = 0; i < $3; i++ )); do
+for (( i = 0; i < $3; )); do
+  for (( j = 0; j < $njob-1; i++&&j++)); do
+    $1 $2 $i &
+  done
+  if [ ${i} -eq $3 ];then
+    break
+  fi
+  i=$((i+=1))
   $1 $2 $i
+  # echo outside
 done
