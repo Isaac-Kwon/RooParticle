730b726e3d1259bce1e1ee97b8ac22b392831eb1
Minjae Isaac Kwon <minjae.isaac.kwon@gmail.com>
2019-03-03 17:54:55 +0900

    multijob: watchdog refresh rate

--- macro/runmulti.py
+++ macro/runmulti.py
@@ -28,6 +28,7 @@ class JobManager:
                     sleep(1.0);
                 for i in range(len(self.joblist)):
                     self.DeleteKilled()
+                sleep(2.0)
         except KeyboardInterrupt:
             print("KILL ALL PROCESS")
             for i in range(len(self.joblist)):
