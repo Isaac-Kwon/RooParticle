976486e83eb8c890e481e4e635e628dd9471c248
Minjae Kwon <mkwon@cern.ch>
2019-05-30 22:11:48 +0900

    Collider Experiement 1st production update

--- test/main_collider_HeHe.cpp
+++ test/main_collider_HeHe.cpp
@@ -10,6 +10,7 @@
 #include "TROOT.h"
 #include "TTree.h"
 #include "TFile.h"
+#include "TRandom3.h"
 
 class ColliderExperiment{
 public:
@@ -31,12 +32,12 @@ void ColliderExperiment::makeEvent(particle* p){
   currentEvent->AddParticle(p);
 }
 
-Float_t RandomFloat(Float_t a, Float_t b) {
-    Float_t random = ((Float_t) rand()) / (Float_t) RAND_MAX;
-    Float_t diff = b - a;
-    Float_t r = random * diff;
-    return a + r;
-}
+// Float_t RandomFloat(Float_t a, Float_t b) {
+//     Float_t random = ((Float_t) rand()) / (Float_t) RAND_MAX;
+//     Float_t diff = b - a;
+//     Float_t r = random * diff;
+//     return a + r;
+// }
 
 TVectorD unitv(TVectorD v){
   TVectorD result = TVectorD(3);
@@ -75,7 +76,6 @@ int main(int argc, char** argv){
 
   Float_t imp_min = 0.;
   Float_t imp_max = +1000.;
-  Float_t imp;
 
   EMparticle * p1, * p2;
 
@@ -84,36 +84,48 @@ int main(int argc, char** argv){
 
   Double_t x2_[] = {-1000, 0., 0.,};
 
+  UInt_t randseed = (unsigned int) time(NULL);
+  TRandom3 * rg = new TRandom3(randseed);
+
   Int_t i;
   Int_t j=0;
-  TTree * tree = new TTree("T", "Two Particle Colliding Angle");
 
-  Double_t   imp_tree;
-  Int_t      NPOINT_tree;
-  Double_t   vx1_tree;
-  Double_t   vx2_tree;
-  Double_t * vx1p_tree;
-  Double_t * vx2p_tree;
-  Double_t   SAngle1_tree;
-  Double_t   SAngle2_tree;
-  Double_t   DCA_tree;
-  Double_t   outtime_free;
+  TFile * hfile;
 
-  tree->Branch("imp", &imp_tree, "imp/D");
-  tree->Branch("NPOINT", &NPOINT_tree, "Npoint/I");
-  tree->Branch("x1", & vx1_tree, "x1/D");
-  tree->Branch("x2", & vx2_tree, "x2/D");
-  tree->Branch("x1p", vx1p_tree, "x1p[Npoint]/D");
-  tree->Branch("x2p", vx1p_tree, "x2p[Npoint]/D");
-  tree->Branch("SAngle1", & SAngle1_tree, "angle1/D");
-  tree->Branch("SAngle2", & SAngle2_tree, "angle2/D");
-  tree->Branch("DCA", & DCA_tree, "DCA/D");
+  
+  TString findex;
+  if(argc==0){
+    findex = TString("0");
+  }else{
+    findex = TString(argv[1]);
+  }
+  
+  hfile = new TFile(TString::Format("Data/test_collider_%s.root",argv[1]),"RECREATE");
+  TTree * tree = new TTree("T_Colliding", "Two Particle Colliding Angle");
 
+  Float_t    imp;
+  Int_t      NPOINT;
+  Double_t   vx1;
+  Double_t   vx2;
+  Double_t   SAngle1;
+  Double_t   SAngle2;
+  Double_t   DCA;
+  Double_t   outtime;
 
-  TFile * hfile = new TFile(TString::Format("Data/test_collider_long_JOB_%s_%d.root",argv[1],j),"RECREATE");
+  tree->Branch("imp", &imp, "imp/D");
+  tree->Branch("NPOINT", &NPOINT, "Npoint/I");
+  tree->Branch("x1", & vx1, "x1/D");
+  tree->Branch("x2", & vx2, "x2/D");
+  // tree->Branch("x1p", vx1p, "x1p[Npoint]/D");
+  // tree->Branch("x2p", vx1p, "x2p[Npoint]/D");
+  tree->Branch("SAngle1", & SAngle1, "angle1/D");
+  tree->Branch("SAngle2", & SAngle2, "angle2/D");
+  tree->Branch("DCA", & DCA, "DCA/D");
 
-  for(i=0; i<100 ;i++ ){
-    imp = RandomFloat(imp_min,imp_max);
+  cout<<"Starting Data Production Index "<<findex<<endl;
+  cout<<"RandomSeed : "<<randseed<<endl;
+  for(i=0; i<10000; i++){
+    imp = rg->Uniform(imp_min,imp_max);
     x2_[1] = imp;
     x2 = TVectorD(3,x2_);
 
@@ -122,37 +134,37 @@ int main(int argc, char** argv){
 
     CDE->makeEvent(p2);
     CDE->getEvent()->AddParticle(p1);
-
     CDE->getEvent()->DeriveMAX(1);
 
-    imp_tree = imp;
-    NPOINT_tree = p2->GetPath()->GetMaxNumber();
-    SAngle1_tree = angleXD(p2->GetPath()->GetLastV());
-    SAngle2_tree = angleXD(CDE->getEvent()->getParticle(1)->GetPath()->GetLastV());
-    p2->GetPath()->GetDCA(p1->GetPath(), DCA_tree, outtime_free);
-    vx1_tree = p1->GetPath()->GetLastX()[0];
-    vx2_tree = p2->GetPath()->GetLastX()[0];
+    NPOINT = p2->GetPath()->GetMaxNumber();
+    SAngle1 = angleXD(p2->GetPath()->GetLastV());
+    SAngle2 = angleXD(CDE->getEvent()->getParticle(1)->GetPath()->GetLastV());
+    p2->GetPath()->GetDCA(p1->GetPath(), DCA, outtime);
+    vx1 = p1->GetPath()->GetLastV()[0];
+    vx2 = p2->GetPath()->GetLastV()[0];
 
     tree->Fill();
 
-    if(i%100==0&&argv==0){
-      cout<<argv[0]<<"/"<<i<<":"<<imp_tree<<":"<<SAngle1_tree<<":"<<SAngle2_tree<<":"<<DCA_tree<<":"<<outtime_free<<endl;
+    if(i%100==0){
+      cout<<findex<<endl<<":"<<"/"<<i<<":"<<imp<<":"<<SAngle1<<":"<<SAngle2<<":"<<DCA<<":"<<outtime<<endl;
       cout<<"LAST POS P1: "<<p1->GetPath()->GetLastX()[0]<<endl;
       cout<<"LAST POS P2: "<<p2->GetPath()->GetLastX()[0]<<endl;
+      tree->AutoSave();
     }
       // p2->GetPath()->GetLastV().Print();
       // CDE->getEvent()->getParticle(1)->GetPath()->GetLastV().Print();
+    CDE->offEvent();
+    CDE->delEvent();
   }
-  CDE->offEvent();
-  CDE->delEvent();
+
 
   if(argc==0){
     tree->Print();
   }else{
-    cout<<"JOB END: "<<argv[0]<<endl;
+    cout<<"JOB END: "<<argv[1]<<endl;
   }
   tree->AutoSave();
-  // hfile->Write();
+  hfile->Write();
   hfile->Close();
   delete hfile;
 
