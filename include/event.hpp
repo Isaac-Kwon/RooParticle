33a99b7f0fe7829cfe1d595a0c8a8c2729f71716
Minjae Isaac Kwon <mkwon@cern.ch>
2019-10-27 20:19:22 +0900

    OddEvent Update

--- include/event.hpp
+++ include/event.hpp
@@ -9,6 +9,7 @@
 #include "TString.h"
 #include "TMath.h"
 
+
 #include "particle.hpp"
 #include "force.hpp"
 #include "eventVolume.hpp"
@@ -22,7 +23,7 @@ class event{
 public:
   event();
   event(event &e);
-  ~event();
+  virtual ~event();
 
   //Setup Methods
   particle* AddParticle(particle* particle_){particles.push_back(particle_); nparticle++; return particle_;}
@@ -31,13 +32,14 @@ public:
   void SetVolume(eventVolume * volume_){volume = volume_;}
 
   //DerivingMethod
-  void DeriveDT(Double_t dt = 1);
+  virtual void DeriveDT(Double_t dt = 1);
   void DeriveDTN(Int_t n = 1, Double_t dt = 1);
   void DeriveMAX(Double_t dt = 1);
   void DeriveInspect(Int_t iperiod=1, Bool_t verbose=kFALSE, Int_t vperiod=1000);
 
   //GetMethod
   particle * getParticle(Int_t index);
+  force * getForce(Int_t index);
   Int_t getNParticle(){return nparticle;}
   Int_t getNForce(){return nforce;}
 
@@ -51,22 +53,25 @@ public:
 
   Bool_t SetInitial();
 
+  // void SetDeriver(deriver * deriver_, Bool_t delete_deriver=kTRUE);
+
+  // friend deriver;
+
 protected:
   inspector* getInspector(Int_t index){return inspectors[index];}
   Int_t getNInspector(){return inspectors.size();}
-
-private:
   std::vector<particle*> particles;
   std::vector<force*> forces;
-  eventVolume * volume;
 
+  Int_t nparticle = 0;
+  Int_t nforce = 0;
+
+private:
+  eventVolume * volume;
   event * pEvent;
   Bool_t preDerived = kFALSE;
   // Bool_t preTuning();
   
-  Int_t nparticle = 0;
-  Int_t nforce = 0;
-
   std::vector<inspector*> inspectors;
 };
 
