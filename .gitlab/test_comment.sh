bf60864006bdf4098cac7078421f10e7720fbda2
Minjae Kwon <minjae.kwon@cern.ch>
2019-10-28 13:47:34 +0000

    Update test_comment.sh

--- .gitlab/test_comment.sh
+++ .gitlab/test_comment.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl --request POST --header "PRIVATE-TOKEN:$GITLAB_API_KEY" --form "note=Nice picture man!" https://gitlab.cern.ch/api/v4/projects/${CI_PROJECT_PATH//\//%2F}/repository/commits/$CI_BUILD_REF/comments
+curl -v --request POST --header "PRIVATE-TOKEN:$GITLAB_API_KEY" --form "note=Nice picture man!" https://gitlab.cern.ch/api/v4/projects/${CI_PROJECT_PATH//\//%2F}/repository/commits/$CI_BUILD_REF/comments
