682bfe888248f3f7dd582299bc036573c2f0e391
Minjae Isaac Kwon <mkwon@cern.ch>
2019-10-31 17:37:39 +0900

    Update Issue Upload, SHA is included.

--- .gitlab/upload_qa.py
+++ .gitlab/upload_qa.py
@@ -10,18 +10,20 @@ print(argv)
 
 g = gitlab(argv[1], argv[2], argv[3])
 adr = g.uploadFile(argv[4],True)
+commit_sha = argv[5]
 
 adr = adr.decode('utf8').replace("'", '"')
 adr = json.loads(adr)
 
 content = """
 Test QA
+COMMIT SHA: %s
 
 Produced MC File (ROOT): %s
 
-"""%(adr["markdown"])
+"""%(commit_sha, adr["markdown"])
 
-g.postIssue("Test QA", description=content, labels="Test QA, QA")
+g.postIssue("Test QA %s"%(commit_sha), description=content, labels="Test QA, QA")
 
 
 
