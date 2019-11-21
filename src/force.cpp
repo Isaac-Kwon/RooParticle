5a7c274b2f5a651eb5078413377c5c7586cdbc90
Minjae Isaac Kwon <mkwon@cern.ch>
2019-11-22 05:17:49 +0900

    Unify the numbers (not define two times)

--- src/force.cpp
+++ src/force.cpp
@@ -26,8 +26,7 @@ Double_t coulombForce::Potential(particle* p1_, particle* p2_){
   EMparticle* p1 = (EMparticle *) p1_;
   EMparticle* p2 = (EMparticle *) p2_;
   Double_t len, q1, q2;
-  len = (p1->GetX() - p2->GetX()).Mag();
-  TVector3 unit = (p1->GetX() - p2->GetX())*(1/len);
+  len = ((p1->GetX())-(p2->GetX())).Mag();
   q1 = p1->GetQ();
   q2 = p2->GetQ();
   Double_t temppotential = coulombK * q1 * q2 * (1/len);
